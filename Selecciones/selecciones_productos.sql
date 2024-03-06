fd3a02856121216fecc6cc952333786b05013116
AlvaroH20 <alvarodorian@outlook.es>
2024-03-06 14:28:30 -0500

    selecciones terminadas en las diferentes tablas

--- Selecciones/selecciones_productos.sql
+++ Selecciones/selecciones_productos.sql
@@ -32,6 +32,4 @@ select * from productos
 --selects
 select * from productos where nombre like'Q%'
 select * from productos where descripcion is null
-select * from productos where precio between '2' and '3'
-
-
+select * from productos where precio between money(2) and money(3)
---
+++ Selecciones/selecciones_registro_entradas.sql
@@ -0,0 +1,34 @@
+drop table registros_entrada
+create table registros_entrada(
+	codigo_registro int not null,
+	cedula_empleado char(10) not null,
+	fecha date not null,
+	hora time not NULL,
+	constraint registros_entrada_pk primary key (codigo_registro)
+)
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(1,'1234567891','01/02/2024','09:30')
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(2,'1234567892','02/01/2024','10:30')
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(3,'1234567893','03/02/2024','11:00')
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(4,'1234567894','04/02/2024','08:28')
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(5,'1234567895','05/01/2024','13:00')
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(6,'1234567896','06/02/2024','15:30')
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(7,'1234567897','07/01/2024','16:00')
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(8,'1234567898','08/02/2024','09:00')
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(9,'1234567899','09/02/2024','07:21')
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(10,'1234567810','10/01/2024','14:20')
+
+select * from registros_entrada
+--selecciones
+select cedula_empleado,fecha,hora from registros_entrada
+select * from registros_entrada where hora between '07:00' and '14:00'
+select * from registros_entrada where hora > '08:00'
