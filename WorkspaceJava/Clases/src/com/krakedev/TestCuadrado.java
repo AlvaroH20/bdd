e21da0c6f582089daf61c91c845b5bf341575c4a
AlvaroH20 <alvarodorian@outlook.es>
2024-03-07 20:15:55 -0500

    Funciones calcular area y calcular perimetro de cuadrado funcionando

---
+++ WorkspaceJava/Clases/src/com/krakedev/TestCuadrado.java
@@ -0,0 +1,37 @@
+package com.krakedev;
+
+public class TestCuadrado {
+
+	public static void main(String[] args) {
+		Cuadrado cua1=new Cuadrado();
+		Cuadrado cua2=new Cuadrado();
+		Cuadrado cua3=new Cuadrado();
+		
+		double areaC1;
+		double perimetroC1;
+		double areaC2;
+		double perimetroC2;
+		double areaC3;
+		double perimetroC3;
+		
+		cua1.lado=4;
+		cua2.lado=3;
+		cua3.lado=2;
+		
+		areaC1=cua1.calcularArea();
+		perimetroC1=cua1.calcularPerimetro();
+		areaC2=cua2.calcularArea();
+		perimetroC2=cua2.calcularPerimetro();
+		areaC3=cua3.calcularArea();
+		perimetroC3=cua3.calcularPerimetro();
+		
+		System.out.println("Area: "+areaC1);
+		System.out.println("Perimetro: "+perimetroC1);
+		System.out.println("Area: "+areaC2);
+		System.out.println("Perimetro: "+perimetroC2);
+		System.out.println("Area: "+areaC3);
+		System.out.println("Perimetro: "+perimetroC3);
+		
+	}
+
+}
