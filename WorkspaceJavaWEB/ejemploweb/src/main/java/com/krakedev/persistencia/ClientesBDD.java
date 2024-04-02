0869431f3e2f91595592927b9796ec95054115d4
AlvaroH20 <alvarodorian@outlook.es>
2024-04-02 08:34:33 -0500

    Reto 41

--- WorkspaceJavaWEB/ejemploweb/src/main/java/com/krakedev/persistencia/ClientesBDD.java
+++ WorkspaceJavaWEB/ejemploweb/src/main/java/com/krakedev/persistencia/ClientesBDD.java
@@ -2,7 +2,9 @@ package com.krakedev.persistencia;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.krakedev.entidades.Cliente;
 import com.krakedev.excepciones.KrakeDevException;
@@ -21,7 +23,7 @@ public class ClientesBDD {
             ps.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
-            throw new KrakeDevException("Error al insertar el cliente");
+            throw new KrakeDevException("Error al insertar el cliente. Detalle: "+e.getMessage());
         } finally {
             if (con != null) {
                 try {
@@ -45,7 +47,7 @@ public class ClientesBDD {
 
         } catch (SQLException e) {
             e.printStackTrace();
-            throw new KrakeDevException("Error al actualizar el cliente");
+            throw new KrakeDevException("Error al actualizar el cliente. Detalle: "+e.getMessage());
         } finally {
             if (con != null) {
                 try {
@@ -56,4 +58,95 @@ public class ClientesBDD {
             }
         }
      }
+    public ArrayList<Cliente> recuperarTodos() throws KrakeDevException{
+    	ArrayList<Cliente> clientes=new ArrayList<Cliente>();
+    	Connection con=null;
+    	PreparedStatement ps=null;
+    	ResultSet rs=null;
+    	Cliente cliente=null;
+    	try {
+			con=ConexionBDD.obtenerConexion();
+			ps=con.prepareStatement("select cedula,nombre,numeroHijos from clientes");
+			rs=ps.executeQuery();
+			
+			while(rs.next()) {
+				String cedula=rs.getString("cedula");
+				String nombre=rs.getString("nombre");
+				int numeroHijos=rs.getInt("numeroHijos");
+				cliente = new Cliente(cedula,nombre,numeroHijos);
+				clientes.add(cliente);
+			}
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			throw e;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new KrakeDevException("Error al consultar. Detalle:"+e.getMessage());
+		}
+    	return clientes;
+    }
+    
+    
+    public Cliente buscarPorPK(String cedulaB) throws KrakeDevException{
+    	Connection con=null;
+    	PreparedStatement ps=null;
+    	ResultSet rs=null;
+    	Cliente cliente=null;
+    	try {
+			con=ConexionBDD.obtenerConexion();
+			ps=con.prepareStatement("select cedula,nombre,numeroHijos from clientes where cedula =?");
+			ps.setString(1, cedulaB);
+			rs=ps.executeQuery();
+			
+			if(rs.next()) {
+				String cedula=rs.getString("cedula");
+				String nombre=rs.getString("nombre");
+				int numeroHijos=rs.getInt("numeroHijos");
+				cliente = new Cliente(cedula,nombre,numeroHijos);
+			}
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			throw e;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new KrakeDevException("Error al consultar. Detalle:"+e.getMessage());
+		}
+    	return cliente;
+    }
+    public ArrayList<Cliente> buscarPorNH(int numeroH) throws KrakeDevException{
+    	ArrayList<Cliente> clientes=new ArrayList<Cliente>();
+    	Connection con=null;
+    	PreparedStatement ps=null;
+    	ResultSet rs=null;
+    	Cliente cliente=null;
+    	try {
+			con=ConexionBDD.obtenerConexion();
+			ps=con.prepareStatement("select cedula,nombre,numeroHijos from clientes where numeroHijos >=?");
+			ps.setInt(1, numeroH);
+			rs=ps.executeQuery();
+			
+			while(rs.next()) {
+				String cedula=rs.getString("cedula");
+				String nombre=rs.getString("nombre");
+				int numeroHijos=rs.getInt("numeroHijos");
+				cliente = new Cliente(cedula,nombre,numeroHijos);
+				clientes.add(cliente);
+			}
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			throw e;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new KrakeDevException("Error al consultar. Detalle:"+e.getMessage());
+		}finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+		}
+    	return clientes;
+    }
 }
--- WorkspaceJavaWEB/ejemploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
+++ WorkspaceJavaWEB/ejemploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
@@ -1,10 +1,13 @@
 package com.krakedev.servicios;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -27,7 +30,7 @@ public class ServicioClientes {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Cliente buscar() {
-		Cliente cliente=new Cliente("1714658945","Alvaro");
+		Cliente cliente=new Cliente("1714658945","Alvaro",20);
 		return cliente;
 	}
 	@Path("insertar")
@@ -59,4 +62,47 @@ public class ServicioClientes {
 			return Response.serverError().build();
 		}
 	}
+	@Path("all")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response obtenerClientes(){
+		ClientesBDD cli = new ClientesBDD();
+		ArrayList<Cliente> clientes=null;
+		try {
+			clientes = cli.recuperarTodos();
+			return Response.ok(clientes).build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build(); 
+		}
+	}
+	@Path("buscarPorCedula/{cedulaParam}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response buscarPorCedula(@PathParam("cedulaParam") String cedula){
+		ClientesBDD cli = new ClientesBDD();
+		Cliente cliente=null;
+		try {
+			cliente = cli.buscarPorPK(cedula);
+			return Response.ok(cliente).build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build(); 
+		}
+	}
+	@Path("buscarPorHijos/{hijosParam}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response buscarPorHijos(@PathParam("hijosParam") int hijos){
+		ClientesBDD cli = new ClientesBDD();
+		ArrayList<Cliente> clientes = null;
+		try {
+			clientes = cli.buscarPorNH(hijos);
+			return Response.ok(clientes).build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build(); 
+		}
+	}
+	
 }
